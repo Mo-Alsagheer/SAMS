2bb73f9b3b69847d2f8950e67b880b3de9fd8fd4
Haneen Rabea <haneenkhalaf52@gmail.com>
2026-03-05 21:14:35 +0200

    save

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Quiz from "./user/quiz/pages/Quiz"
 import QuizResult from "./user/quiz/pages/QuizResult";
+import HomePage from "./user/homePage/pages/HomePage";
 function App() {
   return (
     <Routes>
       <Route path="/quiz" element={<Quiz />} />
       <Route path="/result" element={<QuizResult />} />
+       <Route path="/" element={<HomePage />} />
     </Routes>
   );
 }
---
+++ client/src/user/homePage/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import Footer from "../../../components/layout/Footer";
+import Navbar from "../../../components/layout/Navbar";
+import HeroSection from "../components/HeroSection";
+
+function HomePage() {
+  return (
+    <div>
+    <Navbar/>
+      <HeroSection/>
+      <Footer/>
+    </div>
+  )
+}
+export default HomePage;
