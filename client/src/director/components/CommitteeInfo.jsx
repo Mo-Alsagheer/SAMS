8ba9ddac6f5765b5c4c92c13ffae1df6e77eb1e2
mariam <mariam.nasser961@gmail.com>
2026-03-13 15:01:23 +0200

    add shared component

--- client/src/director/components/CommitteeInfo.jsx
+++ client/src/director/components/CommitteeInfo.jsx
@@ -1,24 +1,22 @@
-import { Button } from "@/components/ui/button";
+import EditableTextArea from "@/components/shared/EditableTextArea";
 
 function CommitteeInfo({ committee }) {
+
+  const handleSaveDescription = (newText) => {
+    console.log("save to api", newText);
+  };
+
   return (
     <div className="bg-card border rounded-lg p-6 space-y-3">
       <h2 className="text-xl font-semibold">{committee.name}</h2>
 
       <p className="text-muted-foreground">{committee.season}</p>
 
-      <div className="space-y-2">
-        <p className="font-medium">Description</p>
-
-        <div className="flex gap-3 items-start">
-          <p className="bg-muted p-3 rounded-md flex-1">
-            {committee.description}
-          </p>
-          <Button className={"bg-sky-600 hover:bg-sky-800 cursor-pointer"}>
-            Edit
-          </Button>
-        </div>
-      </div>
+      <EditableTextArea
+        label="Description"
+        value={committee.description}
+        onSave={handleSaveDescription}
+      />
     </div>
   );
 }
