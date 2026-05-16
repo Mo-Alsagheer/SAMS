81f1cb6081e5c1a999241a8641ed098e66590a9d
sohila ibrahim <sohila.ibrahim@shezlong.com>
2026-05-16 16:17:38 +0300

    feat: enable role-based access control for roadmap operations

--- server/src/modules/roadmap/roadmap.controller.ts
+++ server/src/modules/roadmap/roadmap.controller.ts
@@ -7,19 +7,24 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { RoadmapService } from './roadmap.service';
 import { CreateRoadmapDto } from './dto/create-roadmap.dto';
 import { UpdateRoadmapDto } from './dto/update-roadmap.dto';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Role } from 'src/common/constants/role.enum';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/common/guards/roles.guard';
 
 @ApiTags('roadmap')
 @Controller('roadmap')
 export class RoadmapController {
   constructor(private readonly roadmapService: RoadmapService) {}
 
-  // @UseGuards(JwtAuthGuard, RolesGuard)
-  // @Roles(Role.DIRECTOR, Role.EXECUTIVE)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.DIRECTOR, Role.EXECUTIVE)
   @Post()
   @ApiOperation({ summary: 'Create a roadmap associated with a director' })
   create(@Body() createRoadmapDto: CreateRoadmapDto) {
@@ -36,8 +41,8 @@ export class RoadmapController {
     return this.roadmapService.findOne(id);
   }
 
-  // @UseGuards(JwtAuthGuard, RolesGuard)
-  // @Roles(Role.DIRECTOR, Role.EXECUTIVE)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.DIRECTOR, Role.EXECUTIVE)
   @Patch(':committeeId')
   update(
     @Param('committeeId') committeeId: string,
@@ -49,8 +54,8 @@ export class RoadmapController {
     );
   }
 
-  // @UseGuards(JwtAuthGuard, RolesGuard)
-  // @Roles(Role.DIRECTOR, Role.EXECUTIVE)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.DIRECTOR, Role.EXECUTIVE)
   @Patch('by-id/:id')
   updateById(
     @Param('id', ParseIntPipe) id: number,
@@ -59,8 +64,8 @@ export class RoadmapController {
     return this.roadmapService.update(id, updateRoadmapDto);
   }
 
-  // @UseGuards(JwtAuthGuard, RolesGuard)
-  // @Roles(Role.DIRECTOR, Role.EXECUTIVE)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.DIRECTOR, Role.EXECUTIVE)
   @Patch(':id/assign/:committeeId')
   assignCommittee(
     @Param('id', ParseIntPipe) id: number,
@@ -69,15 +74,15 @@ export class RoadmapController {
     return this.roadmapService.assignCommittee(id, committeeId);
   }
 
-  // @UseGuards(JwtAuthGuard, RolesGuard)
-  // @Roles(Role.DIRECTOR, Role.EXECUTIVE)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.DIRECTOR, Role.EXECUTIVE)
   @Patch(':id/unassign')
   unassignCommittee(@Param('id', ParseIntPipe) id: number) {
     return this.roadmapService.unassignCommittee(id);
   }
 
-  // @UseGuards(JwtAuthGuard, RolesGuard)
-  // @Roles(Role.DIRECTOR, Role.EXECUTIVE)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.DIRECTOR, Role.EXECUTIVE)
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.roadmapService.remove(id);
